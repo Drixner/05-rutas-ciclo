0a56e17b3c59475f287226992b1c6b9751485a6f
Drixner <drixnerrafael1@gmail.com>
2024-02-23 00:45:12 -0500

    Add lifecycle hooks to AboutPage.vue

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -4,3 +4,40 @@
     <p>This is the about page</p>
   </div>
 </template>
+<script>
+export default {
+  beforeCreate() {
+    console.log("AboutPage beforeCreate");
+  },
+  created() {
+    console.log("AboutPage created");
+  },
+  beforeMount() {
+    console.log("AboutPage beforeMount");
+  },
+  mounted() {
+    console.log("AboutPage mounted");
+  },
+  beforeUpdate() {
+    console.log("AboutPage beforeUpdate");
+  },
+  updated() {
+    console.log("AboutPage updated");
+  },
+  beforeUnmount() {
+    console.log("AboutPage beforeUnmount");
+  },
+  unmounted() {
+    console.log("AboutPage unmounted");
+  },
+  errorCaptured() {
+    console.log("AboutPage errorCaptured");
+  },
+  renderTracked() {
+    console.log("AboutPage renderTracked");
+  },
+  renderTriggered() {
+    console.log("AboutPage renderTriggered");
+  },
+};
+</script>
