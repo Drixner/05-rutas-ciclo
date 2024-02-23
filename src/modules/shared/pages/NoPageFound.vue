9a1b83896f4874938097eafef00797fb2cdb7e16
Drixner <drixnerrafael1@gmail.com>
2024-02-22 21:33:53 -0500

    Add error message to NoPageFound component

--- src/modules/shared/pages/NoPageFound.vue
+++ src/modules/shared/pages/NoPageFound.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
     <h1>404 - Not Found</h1>
+    <h2>
+      The page you are looking for might have been removed, had its name changed
+      or is temporarily unavailable.
+    </h2>
   </div>
 </template>
