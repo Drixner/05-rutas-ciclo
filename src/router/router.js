f670124e5ecfa4e70ab819d78c455020e1c2cac0
Drixner <drixnerrafael1@gmail.com>
2024-03-01 00:11:30 -0500

    Refactor Pokemon page route

--- src/router/router.js
+++ src/router/router.js
@@ -15,17 +15,14 @@ const routes = [
         /*webpackChunkName: "AboutPage"*/ "@/modules/pokemon/pages/AboutPage"
       ),
   },
-  {
-    path: "/:id",
-    name: "Pokemon-id",
-    component: () =>
-      import(
-        /*webpackChunkName: "PokemonPage"*/ "@/modules/pokemon/pages/PokemonPage"
-      ),
-    props: (route) => {
-      const id = Number(route.params.id);
-      return isNaN(id) ? { id: 1 } : { id };
-    },
+  { 
+    path: 'pokemonid/:id',
+    name: 'pokemon-id',
+    component: () => import(/* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage'),
+    props: ( route ) => {
+        const id = Number( route.params.id );
+        return isNaN( id ) ? { id: 1 } : { id }
+    }
   },
   //   pagina 404
   {
